4ffea8eb9895bffb2889bbe27114d3973560e884
AlexanderWang72839 <89221464+AlexanderWang72839@users.noreply.github.com>
2022-05-10 12:38:56 -0500

    HEHEHEHEHHEHEE

---
+++ PosterProject/Poster.java
@@ -0,0 +1,61 @@
+
+/**
+ * Makes Poster project
+ *
+ * @author Alexander Wang
+ * @version 5/10/22
+ */
+
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
+public class Poster {
+    public static void main(String[] args) {
+        
+    }
+    
+    /**
+     * Mirrors the image verticaly along its center
+     * @param pic the picture to be modified
+     */
+    public static void mirrorVertical(Picture pic) {
+        
+    }
+    
+    /**
+     * Mirrors the image horizontaly along its center
+     * @param pic the picture to be modified
+     */
+    public static void mirrorHorizontal(Picture pic) {
+        
+    }
+    
+    /**
+     * Mirrors the image diagnaly along y = x
+     * @param pic the picture to be modified
+     */
+    public static void mirrorDiagnaly(Picture pic) {
+        
+    }
+    
+    /**
+     * Grayscales the image
+     * @param pic the picture to be modified
+     */
+    public static void grayscale(Picture pic) {
+        
+    }
+    
+    /**
+     * Blures the image
+     * @param pic  the picture to be modified
+     */
+    public static void blur(Picture pic) {
+        
+    }
+    
+    /**
+     * Fades the picture to white
+     */
+}
--- PosterProject/PosterIntro.java
+++ PosterProject/PosterIntro.java
@@ -12,11 +12,26 @@ import java.util.List;
 public class PosterIntro {
     public static void main(String[] args) {
         Picture apic = new Picture("images/BLITZCRANK.jpg");
+        Picture temple = new Picture("images/temple.jpg");
+        
+        Picture sourcePic = new Picture("images/caterpillar.jpg");
+        Picture targetPic = new Picture("images/640x480.jpg");
+        
+        
+        temple.explore();
         
         something(apic);
-        mirrorTempleVertical(apic);
+        mirrorVertical(apic);
+        //mirrorTemple(16, 23, 276, 108, temple);
+        mirrorTemple(16, 23, 282, 108, temple);
         
-        apic.explore();
+        //apic.explore();
+        temple.explore();
+        
+        
+        targetPic.explore();
+        copyTo(sourcePic, targetPic, 0, 0);
+        targetPic.explore();
     }
     
     public static void something (Picture apic) {
@@ -33,7 +48,7 @@ public class PosterIntro {
         }
     }
     
-    public static void mirrorTempleVertical(Picture apic) {
+    public static void mirrorVertical(Picture apic) {
         int height = apic.getHeight();
         int width = apic.getWidth();
         int mirrorPoint = width / 2;
@@ -57,9 +72,46 @@ public class PosterIntro {
         Pixel temp;
         
         for (int x = 0 ; x <= width ; x++) {
-            for (int y = 0 ; y < height ; y++) {
-                spot = apic.getPixel(x, y % height);
+            for (int y = 0 ; y < height * x ; y++) {
+                temp = 
+                spot = apic.getPixel(x, y);
             }
+        }
+    }
+    
+    public static void mirrorTemple(int startX, int startY, int endX, int endY, Picture apic) {
+        int width = apic.getWidth();
+        int height = apic.getHeight();
+        Pixel leftPixel = null;
+        Pixel rightPixel = null;
+        
+        for (int x = startX ; x <= endX; x++) {
+            for (int y = startY ; y < endY ; y++) {
+                leftPixel = apic.getPixel(x, y);
+                rightPixel = apic.getPixel(width - x - 1, y);
+                
+                rightPixel.setColor(leftPixel.getColor());
+            }
+        }
+    }
+    
+    /**
+     * copy Source to Target at pos x, y
+     * @param Picture sorce, taget, x, y
+     */
+    public static void copyTo(Picture source, Picture target, int xOffset, int yOffset) {
+        int height = source.getHeight();
+        int width = source.getWidth();
+        Pixel t = null;
+        Pixel s = null;
+        
+        for (int x = 0 ; x < width ; x++) {
+           for (int y = 0 ; y < height ; y++) {
+                t = target.getPixel(x, y);
+                s = source.getPixel(x, y);
+                
+                t.setColor(s.getColor());
+           }    
         }
     }
 }
